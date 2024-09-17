b794d4931800184701180fb06a312213b283059e
wheeler-cs <wheelercs@protonmail.com>
2024-09-17 12:32:36 -0500

    Added Assembly Dump
    Added the ability to both store the disassembled mnemonics and dump them
    to an output file for examination.
    
    Signed-off-by: wheeler-cs <wheelercs@protonmail.com>

--- src/Disassembler.py
+++ src/Disassembler.py
@@ -1,3 +1,8 @@
+'''
+@package Disassembler
+
+'''
+
 from capstone import Cs, CS_ARCH_X86, CS_MODE_32, CS_MODE_64
 from pefile import PE
 from sys import argv
@@ -14,6 +19,7 @@ class Disassembler(object):
         self.disassembler = Cs(CS_ARCH_X86, CS_MODE_32)
         self.textSecStart = 0
         self.textSecEnd   = 0
+        self.disasmData   = list()
 
         self.getTextSection()
 
@@ -27,14 +33,21 @@ class Disassembler(object):
 
 
 
-    def disassemble(self, printAddress: bool = False) -> None:
+    def disassemble(self, includeAddress: bool = False) -> None:
         exeCode = self.executable.get_memory_mapped_image()[self.textSecStart:self.textSecEnd]
         epVirtualAddress = self.executable.OPTIONAL_HEADER.ImageBase + self.textSecStart
         for instruction in self.disassembler.disasm(exeCode, epVirtualAddress):
-            if printAddress:
-                print(f"{hex(instruction.address)}: {instruction.mnemonic} {instruction.op_str.replace(' ', '')}")
+            if includeAddress:
+                self.disasmData.append(f"{hex(instruction.address)}: {instruction.mnemonic} {instruction.op_str.replace(' ', '')}")
             else:
-                print(f"{instruction.mnemonic} {instruction.op_str.replace(' ', '')}")
+                self.disasmData.append(f"{instruction.mnemonic} {instruction.op_str.replace(' ', '')}")
+    
+
+
+    def dumpAssembly(self, outputFile: str = "out.asm") -> None:
+        with open(outputFile, "w+") as disasmWrite:
+            for instruction in self.disasmData:
+                disasmWrite.write(f"{instruction}\n")
 
 
 
@@ -46,4 +59,5 @@ class Disassembler(object):
 # == Main ==============================================================================================================
 if __name__ == "__main__":
     disasm = Disassembler(argv[1])
-    disasm.disassemble(True)
+    disasm.disassemble()
+    disasm.dumpAssembly("data/out.disasm")
