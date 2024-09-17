dcfca2962e2f68a7759db6a575e71e93df6da1cc
wheeler-cs <wheelercs@protonmail.com>
2024-09-17 11:30:14 -0500

    Initial Disassembler
    Signed-off-by: wheeler-cs <wheelercs@protonmail.com>

---
+++ src/Disassembler.py
@@ -0,0 +1,51 @@
+from capstone import Cs, CS_ARCH_X86, CS_MODE_32
+from pefile import PE
+from sys import argv
+
+
+
+# == Disassembler Class ================================================================================================
+# See https://github.com/erocarrera/pefile/blob/wiki/UsageExamples.md#introduction
+class Disassembler(object):
+    def __init__(self, inputFile: str) -> None:
+        self.exeName      = inputFile
+        self.executable   = PE(argv[1], fast_load=True)
+        self.disassembler = Cs(CS_ARCH_X86, CS_MODE_32)
+        self.textSecStart = 0
+        self.textSecEnd   = 0
+
+        self.getTextSection()
+
+
+
+    def getTextSection(self) -> None:
+        for section in self.executable.sections:
+            if(section.Name == b".text\0\0\0"):
+                self.textSecStart = section.VirtualAddress
+                self.textSecEnd = section.VirtualAddress + section.Misc_VirtualSize
+
+
+
+    def disassemble(self) -> None:
+        exeCode = self.executable.get_memory_mapped_image()[self.textSecStart:self.textSecEnd]
+        for instruction in self.disassembler.disasm(exeCode, self.textSecStart):
+            print(f"{hex(instruction.address)}: {instruction.mnemonic} {instruction.op_str}")
+        '''
+        entryPoint = self.executable.OPTIONAL_HEADER.AddressOfEntryPoint
+        epVirtualAddress = self.executable.OPTIONAL_HEADER.ImageBase
+        executableCode = self.executable.get_memory_mapped_image()[entryPoint:entryPoint+1000]
+        for instruction in self.disassembler.disasm(executableCode, epVirtualAddress):
+            print(f"{hex(instruction.address)}: {instruction.mnemonic} {instruction.op_str}")
+        '''
+
+
+
+    def printReport(self) -> None:
+        print(self.executable.dump_info())
+
+
+
+# == Main ==============================================================================================================
+if __name__ == "__main__":
+    disasm = Disassembler(argv[1])
+    disasm.disassemble()
