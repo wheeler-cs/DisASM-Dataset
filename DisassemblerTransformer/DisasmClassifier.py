09fd62338314a59a34752b1d8507d19015bdcfd9
Anna <wheelercs@protonmail.com>
2024-12-28 12:14:47 -0600

    Documentation and Standalone Classifier
    Added documentation to the DisasmClassifier.py file to describe how the class works.
    
    Also implemented the ability to run the module without having to import it from another file. This allows for using the classifier or encoder separate separate from an external script, albiet with some limited functionality.
    
    Signed-off-by: Anna <wheelercs@protonmail.com>

--- DisassemblerTransformer/DisasmClassifier.py
+++ DisassemblerTransformer/DisasmClassifier.py
@@ -1,14 +1,50 @@
+'''
+@package DisasmClassifier
+
+Module providing facilities for leveraging a pre-trained model in the task of assembly classification.
+'''
+
+
+import argparse
+import logging
 import os
+import tensorflow as tf
 from transformers import AutoConfig, TFAutoModelForSequenceClassification, AutoTokenizer
 
-class DisasmClassifier():
+
+# == DisasmClassifier ==================================================================================================
+class DisasmClassifier(object):
+    '''
+    Classifier for determining the associated classification of a given disassembly file. This class utilizes a
+    sequence-based classifier derived from the Facebook AI RoBERTa model available on Hugging Face.
+    '''
+
     def __init__(self, modelType: str) -> None:
-        self.model              = TFAutoModelForSequenceClassification.from_pretrained(modelType)
-        self.tokenizer          = AutoTokenizer.from_pretrained(modelType)
-        self.config             = AutoConfig.from_pretrained(modelType)
+        '''
+        Class initializer.
+
+        @param self Pointer to calling class instance.
+        @param modelType String defining the model from Hugging Face to use for classification. It is recommended that
+               this is a model that has been pre-trained on the associated classification task.
+        '''
+        self.model     = TFAutoModelForSequenceClassification.from_pretrained(modelType)
+        self.tokenizer = AutoTokenizer.from_pretrained(modelType)
+        self.config    = AutoConfig.from_pretrained(modelType)
     
 
     def classifyInput(self, filePath: str) -> None | str:
+        '''
+        Utilize a pre-trained model to obtain a classification for a given disassembly file.
+
+        @param self Pointer to calling class instance.
+        @param filePath Path to disassembly file that will be classified.
+
+        @returns String representation of classification determined by pre-trained model.
+        @retval None Path provided is a directory.
+
+        @pre All member variables of calling class instance have been properly initialized.
+        @see self.__init__()
+        '''
         # Make sure what was passed in is actually a file
         if os.path.isdir(filePath):
             return None
@@ -24,7 +60,19 @@ class DisasmClassifier():
             return self.config.id2label[prediction[0]]
 
 
-    def outputEncoder(self, filePath: str) -> None:
+    def outputEncoder(self, filePath: str) -> None | tf.Tensor:
+        '''
+        Utilize a pre-trained model in encoder-only mode to generate a tensor representation of an assembly file.
+
+        @param self Pointer to calling class instance.
+        @param filePath Path to disassembly file to be converted into a tensor.
+
+        @returns Encoded tensor of provided assembly file.
+        @retval None Path provided is a directory.
+
+        @pre All member variables of calling class instance have been properly initialized.
+        @see self.__init__()
+        '''
         # Make sure what was passed in is actually a file
         if os.path.isdir(filePath):
             return None
@@ -38,3 +86,36 @@ class DisasmClassifier():
             finalTensor = tensors.hidden_states[-1]
             del tensors # Need this or run OOM a lot easier
             return finalTensor
+        
+
+# == main() ============================================================================================================
+if __name__ == "__main__":
+    # Provide command-line arguments so script can run by itself when needed.
+    parser = argparse.ArgumentParser(prog="Disassembly Classifier",
+                                     description="Standalone mode of operation for the DisasmClassifier module.")
+    parser.add_argument("--model",
+                        help="Name of Hugging Face or local model to be used with classifier.",
+                        type=str,
+                        required=True)
+    parser.add_argument("--path",
+                        help="Path of file to be used for classification or encoding.",
+                        type=str,
+                        required=True)
+    parser.add_argument("--mode",
+                        help="The operation the script should perform during execution.",
+                        type=str,
+                        choices=["classifier", "encoder"],
+                        required=True)
+    parser = parser.parse_args()
+
+    # Create class instance
+    disasm = DisasmClassifier(parser.model)
+
+    # Perform desired task
+    match(parser.mode):
+        case "classifier":
+            classification = disasm.classifyInput(parser.path)
+            print(classification)
+        case "encoder":
+            encoderTensor = disasm.outputEncoder(parser.path)
+            print(encoderTensor)
